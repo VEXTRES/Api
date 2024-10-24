f9abc0ea83720516b4a53a6f8eb8d7211618d4d0
Heisenberg <heisenberg@kontramundo.com>
2024-10-23 23:02:58 -0600

    Config Api Client

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+
+Route::middleware('client')->group(function () {
+    Route::resource('users', UserController::class);
+});
