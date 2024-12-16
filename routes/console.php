c4591a626c4365919fe2f4bfe5797192eed9779e
karityna <krinahz@gmail.com>
2024-12-15 18:33:04 -0600

    fix error comando

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Schedule;
 
 
 
-$data = User::where('email', '=', 'uriel.ss@hotmail.com')->first();
+$data = User::where('email', '=', 'uriel.ss@hotmail.com')->firstOrFail();
 
 Schedule::command(SendEmailPdf::class, [$data->id])->saturdays()->at('16:00');
