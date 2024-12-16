9b928442f954c41bd65d1bbdf91572c72cb2097a
karityna <krinahz@gmail.com>
2024-12-15 18:45:10 -0600

    error solucionado

--- routes/console.php
+++ routes/console.php
@@ -6,8 +6,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
+try {
+    $data = User::where('email', '=', 'uriel.ss@hotmail.com')->firstOrFail();
 
-
-// $data = User::where('email', '=', 'uriel.ss@hotmail.com')->firstOrFail();
-
-// Schedule::command(SendEmailPdf::class, [$data->id])->saturdays()->at('16:00');
+    Schedule::command(SendEmailPdf::class, [$data->id])->saturdays()->at('16:00');
+} catch (\Exception $e) {
+}
